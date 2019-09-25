2ed2ff9a35fdcb7d6fa7bd9003bfa2825ce37f03
Aamir Darukhanawalla <aamir@darukhanawalla.com>
2019-09-26 00:00:30 +0530

    Convert logic.py to modular code.

--- Game logic/logic.py
+++ Game logic/logic.py
@@ -5,23 +5,41 @@ from time import sleep
 def clear():
     _ = system('clear' if name =='posix' else 'cls')
 
+def user_plays():
+    while True:
+        clear()
+        print('Runs = {}'.format(runs))
+        print('Choose a number: ', end='')
+        num = int(input())
+        if num in range(7):
+            return num
+        else:
+            print('Illegal move')
+
+def comp_plays():
+    num = randint(0,6)
+    print('Computer plays {}'.format(num))
+    return num
+
+def increment_runs(num):
+    global runs
+    runs += num
+
+def get_out():
+    global out
+    out = True
+    print('Out!')
+    print('Total = {}'.format(runs))
+
 runs = 0
 out = False
 
 while not out:
     clear()
-    print('Runs = {}'.format(runs))
-    print('Choose a number: ', end='')
-    player = int(input())
-    if player not in range(7):
-        print('Illegal move')
+    player = user_plays()
+    comp = comp_plays()
+    if player == comp:
+        get_out()
     else:
-        comp = randint(0,6)
-        print('Computer plays {}'.format(comp))
-        if player == comp:
-            print('Out!')
-            print('Total = {}'.format(runs))
-            out = True
-        else:
-            runs += player
-        sleep(1)
+        increment_runs(player)
+    sleep(1)
