b6f5bc5a62194ec7581cced3ad0b02108a2543ee
Aamir Darukhanawalla <aamir@darukhanawalla.com>
2020-02-22 17:11:38 +0530

    Error fix: Change input variable name in controller.py

--- controller.py
+++ controller.py
@@ -2,8 +2,8 @@ import view
 
 def start():
     view.start_view()
-    input = input()
-    if input == 'y':
+    i = input()
+    if i == 'y':
         return view.start_game()
     else:
         return view.end_view()
