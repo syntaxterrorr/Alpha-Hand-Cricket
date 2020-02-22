6d63c1d5a8ffc328c247f358f3579351be97c6ba
Aamir Darukhanawalla <aamir@darukhanawalla.com>
2020-02-22 17:41:07 +0530

    Add primary key as id to models

--- model.py
+++ model.py
@@ -1,18 +1,29 @@
 class User:
-    pass
+    
+    def __init__(self, user_id, user_name):
+        self.id = user_id
+        self.name = user_name
 
 
 class Game:
-    pass
+    
+    def __init__(self, game_id):
+        self.id = game_id
 
 
 class Turn:
-    pass
+    
+    def __init__(self, turn_id):
+        self.id = turn_id
 
 
 class Toss (Turn):
-    pass
+    
+    def __init__(self, turn_id):
+        super().__init__(turn_id) 
 
 
 class Delivery (Turn):
-    pass
+    
+    def __init__(self, turn_id):
+        super().__init__(turn_id) 
